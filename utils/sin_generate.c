17274849b38a8003618409e398c760914fe4eec0
burin <Micheal.Burin@colorado.edu>
2017-11-06 13:05:43 -0700

    Fixed sin generation amplitude.

--- utils/sin_generate.c
+++ utils/sin_generate.c
@@ -12,20 +12,22 @@
 
 int main() {
 
-printf("/*\n    Quarter sin wave, will mirror the other 3/4\n");
-printf("    360 degrees = 1024 units, this table is 0-255 (1/4)\n");
-printf("    Max Amplitude 127\n*/\n");
 
 
 uint16_t i_max = (PARTS_PER_CYCLE/4);
 
+printf("/*\n    Quarter sin wave, will mirror the other 3/4\n");
+printf("    360 degrees = %d units, this table is 0-%d (1/4)\n",PARTS_PER_CYCLE,i_max-1);
+printf("    Max Amplitude %d\n*/\n",MAX_AMPLITUDE);
+
 printf("const uint8_t t_sine_table[%d] = {\n",i_max);
 
+double max_amplitude = MAX_AMPLITUDE;
 for (uint16_t i=0;i<i_max;i++) {
 
     //Quarter sin wave, will mirror the other 3/4
     double phase =  M_PI_2  * ((double)i / (i_max-1));
-    double tone = 127.0 + (127.0 * sin(phase + M_PI + M_PI_2));
+    double tone = max_amplitude + (max_amplitude * sin(phase + M_PI + M_PI_2));
     uint8_t tone_int = (uint8_t)tone;
 
     printf("%3d",tone_int);
