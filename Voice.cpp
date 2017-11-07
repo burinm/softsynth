a1ffad2ae54bd0ad60ca8c58a88528220214cc8f
burin <Micheal.Burin@colorado.edu>
2017-11-07 12:10:22 -0700

    Made note table static, saved one instance per class! Combined note-on into note gate bit

--- Voice.cpp
+++ Voice.cpp
@@ -19,51 +19,51 @@ extern "C" {
 
 namespace SoftSynth {
 
+/*
+    Formula: round(65536)/ticks) @ 62500.000 Hz
+*/
+
+const uint16_t Voice::note_phase_mult_table[128] = {
+         9,     9,    10,    10,    11,    11,    12,    13,    14,    14,    15,    16,// C-1
+        17,    18,    19,    20,    22,    23,    24,    26,    27,    29,    31,    32,// C0
+        34,    36,    39,    41,    43,    46,    49,    52,    55,    58,    61,    65,// C1
+        69,    73,    77,    82,    87,    92,    97,   103,   109,   116,   123,   130,// C2
+       138,   146,   155,   164,   173,   184,   195,   206,   219,   232,   245,   260,// C3
+       275,   292,   309,   328,   347,   368,   389,   413,   437,   463,   491,   520,// C4
+       551,   584,   618,   655,   694,   735,   779,   825,   874,   926,   981,  1040,// C5
+      1102,  1167,  1237,  1310,  1388,  1470,  1558,  1651,  1749,  1853,  1963,  2080,// C6
+      2203,  2334,  2473,  2620,  2776,  2941,  3116,  3301,  3497,  3705,  3926,  4159,// C7
+      4406,  4669,  4946,  5240,  5552,  5882,  6232,  6602,  6995,  7411,  7851,  8318,// C8
+      8813,  9337,  9892, 10480, 11104, 11764, 12463, 13205
+};
+
 void Voice::init(uint8_t (*f)(uint16_t), envelope_t &e) {
-    ticks = 0;
     phase = 0;
-    previous_phase = 0;
-
     wave_function = f;
-
     current_note = 0;
-    sequencer_flag = 0;
 
     envelope.init(e);
 }
 
 void Voice::step(uint16_t t) {
 
-    phase = t * note_phase_mult_table[current_note];            //?us
-    //phase += note_phase_mult_table[current_note];            //?us
+    phase = t * note_phase_mult_table[GET_NOTE(current_note)];            //?us
     //phase = phase % PARTS_PER_CYCLE; //automatic 16bit rollover
-
-    //if (ticks == note_ticks_table[current_note] +1 ) { ticks = 1; }
-
-    if (sequencer_flag) {
-        //if (//TODO:overflow sync
-            sequencer_flag = 0;
-            envelope.start();                                       //3.44us
-        //}
-    }
-
     envelope.step();                                                //1.92us
-    ticks++;
-
 }
 
 void Voice::startNote(uint8_t midinote) {
-    current_note = midinote;
-    sequencer_flag = 1;
+    SET_NOTE(current_note,midinote);
+    envelope.start();                                       //3.44us
     random_reset();
 } 
 
 void Voice::stopNote() {
-    sequencer_flag = 0;
+    NOTE_OFF(current_note);
     envelope.setState(ADSR_RELEASE);
+    //should we reset envelope here?
 }
 
-#if 1
 uint8_t Voice::sample() {
 uint8_t wave;
 uint16_t amplitude;
@@ -78,8 +78,6 @@ uint16_t amplitude;
 //return (uint8_t)amplitude;
 return (wave);
 }
-#endif
-
 
 void Voice::setWaveform(uint8_t (*f)(uint16_t)) {
     wave_function = f;
--- Voice.h
+++ Voice.h
@@ -12,11 +12,16 @@ extern "C" {
 }
 
 
+
 #include "Envelope.h"
 
 namespace SoftSynth {
+
+
 class Voice {
     public:
+        static const uint16_t note_phase_mult_table[128];
+
         void init(uint8_t (*f)(uint16_t), envelope_t&);
         void startNote(uint8_t midinote);
         void stopNote();
@@ -32,37 +37,24 @@ class Voice {
 #endif
         uint8_t sample();
 
-        uint8_t sequencer_flag;
-
     private:
-        uint16_t ticks;
         uint16_t phase;
-        uint16_t previous_phase;
+
+        //Current note bit 7 is gate, bits 0-6 are note
+        #define NOTE_GATE_BIT   7
+        #define NOTE_GATE_MASK  (1 << NOTE_GATE_BIT)
+        #define NOTE_OFF(u8) ((u8) &= ~NOTE_GATE_MASK)
+        #define NOTE_ON(u8)  ((u8) |= NOTE_GATE_MASK)
+        #define IS_NOTE_ON(u8)  ((u8) & NOTE_GATE_MASK)
+        #define GET_NOTE(u8)    (u8) & ~NOTE_GATE_MASK
+        //Setting note also turns on gate
+        #define SET_NOTE(u8,note)    ((u8) = (note | (1<<NOTE_GATE_BIT)))
         uint8_t current_note;
+
         uint8_t (*wave_function)(uint16_t);
         Envelope envelope;
-
-
-/*
-    Formula: round(65536)/ticks) @ 62500.000 Hz
-*/
-
-const uint16_t note_phase_mult_table[128] = {
-         9,     9,    10,    10,    11,    11,    12,    13,    14,    14,    15,    16,// C-1
-        17,    18,    19,    20,    22,    23,    24,    26,    27,    29,    31,    32,// C0
-        34,    36,    39,    41,    43,    46,    49,    52,    55,    58,    61,    65,// C1
-        69,    73,    77,    82,    87,    92,    97,   103,   109,   116,   123,   130,// C2
-       138,   146,   155,   164,   173,   184,   195,   206,   219,   232,   245,   260,// C3
-       275,   292,   309,   328,   347,   368,   389,   413,   437,   463,   491,   520,// C4
-       551,   584,   618,   655,   694,   735,   779,   825,   874,   926,   981,  1040,// C5
-      1102,  1167,  1237,  1310,  1388,  1470,  1558,  1651,  1749,  1853,  1963,  2080,// C6
-      2203,  2334,  2473,  2620,  2776,  2941,  3116,  3301,  3497,  3705,  3926,  4159,// C7
-      4406,  4669,  4946,  5240,  5552,  5882,  6232,  6602,  6995,  7411,  7851,  8318,// C8
-      8813,  9337,  9892, 10480, 11104, 11764, 12463, 13205
 };
 
-
-};
 
 }
 #endif
